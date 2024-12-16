1963e7ee8f77355eb2b20b7103e06cdcdb04407c
hcc <hcc@gmail.com>
2024-12-16 14:14:24 +0100

    Segundo commit

--- Teclado.java
+++ Teclado.java
@@ -2,4 +2,9 @@ public class Teclado {
     private int tamaño;
     private String idioma;
     private Switch tipoSwitch;
+    
+    public Teclado(int tamanoTeclado, String lenguaje){
+        tamaño = tamanoTeclado;
+        idioma = lenguaje;
+    }
 }
